73e7098a027ce55d862b2d8730005d0fa65a157f
smanthacatonio <84981967+smanthacatonio@users.noreply.github.com>
2022-09-01 09:06:22 -0400

    Adicionar Classes

---
+++ Produto.java
@@ -0,0 +1,9 @@
+package avaliacaotecnicapoo;
+
+public class Produto {
+    
+    private int codigo;
+    private double preco;
+    
+    //Bebidas, Petiscos, Carnes, Aves, Massas e Sobremesas; 
+}

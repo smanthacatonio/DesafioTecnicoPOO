8f1366c746c56b11a4b1db7745e8b302e4ed2eb4
Samantha Ariel Catonio <samanthacatonio@digix.com.br>
2022-09-01 10:32:32 -0400

    criando classes

--- Produto.java
+++
@@ -1,9 +0,0 @@
-package avaliacaotecnicapoo;
-
-public class Produto {
-    
-    private int codigo;
-    private double preco;
-    
-    //Bebidas, Petiscos, Carnes, Aves, Massas e Sobremesas; 
-}
